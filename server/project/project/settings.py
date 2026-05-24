04ead6829013bbc4c238b9a1995148f62b547c21
dara-dsr <dara.hanahaki69@gmail.com>
2026-05-25 03:54:21 +1000

    render config

--- server/project/project/settings.py
+++ server/project/project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['https://velvet-eact.onrender.com']
 
 
 # Application definition

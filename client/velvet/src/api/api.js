6ec538c861728a9ac883f9efb0f583e32959ad32
dara-dsr <dara.hanahaki69@gmail.com>
2026-05-25 13:34:50 +1000

    fix confing

--- client/velvet/src/api/api.js
+++ client/velvet/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api'
+  baseURL: `${API_URL}/api`
 });
 
 api.interceptors.request.use(config => {
